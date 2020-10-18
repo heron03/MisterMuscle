032ceb7aa0f9d50dcdf207039c73ef98f6ffacfd
Matheusmelo3535 <matheusmelo3535@gmail.com>
2020-10-18 17:30:48 -0300

    Tela de cadastro e login de usuario

---
+++ Shared/Usuario.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace ProjetoIntegrador.Shared {
+    
+    public class Usuario {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="O campo nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage="O campo e-mail é obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="O campo CPF é obrigatório")]
+        [MaxLength(11, ErrorMessage = "Seu CPF deve conter no máximo 11 caracteres"),MinLength(11, ErrorMessage = "Seu CPF deve conter no minímo 11 caracteres")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage="O campo senha é obrigatório")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Sua senha deve conter no minímo 6 caracteres")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage="O campo telefone celular é obrigatório")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone(ErrorMessage = "Número de telefone celular inválido")]
+        public string Celular { get; set; }
+        
+        [Required(ErrorMessage="O campo confirme sua senha é obrigatório")]
+        [DataType(DataType.Password)]
+        [CompareProperty("Senha")]
+        public string Confirmarsenha { get; set; }
+    }
+}
